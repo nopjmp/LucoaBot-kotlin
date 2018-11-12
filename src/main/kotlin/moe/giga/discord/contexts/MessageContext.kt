99707961311e734fbd02864bc042010346d0e3d6
Kayla <jmp@0x.gg>
2018-11-11 22:42:52 -0600

    silence platform native warning for message

--- src/main/kotlin/moe/giga/discord/contexts/MessageContext.kt
+++ src/main/kotlin/moe/giga/discord/contexts/MessageContext.kt
@@ -19,7 +19,7 @@ class MessageContext(event: MessageReceivedEvent, val server: ServerContext?) {
     val user: UserContext = UserContext(event.author, server)
     val jda: JDA = event.jda
 
-    val message = event.message
+    val message = event.message!!
 
     private fun sendMessage(message: Message): MessageAction = channel.sendMessage(message)
 
